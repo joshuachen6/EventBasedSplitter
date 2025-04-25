3c14ee5891e2e0f0344346b19998051bca8dcf26
MacMini-05 <macmini-05@Mini-06.local>
2025-04-25 13:14:53 -0500

    Progress, restarting lib

--- include/streamer.h
+++ include/streamer.h
@@ -2,41 +2,4 @@
 #include <metavision/sdk/stream/camera.h>
 
 extern "C" {
-/**
- * @brief Starts the stream
- *
- * @param rawBuffer The raw buffer to stream into
- * @param topLeftBuffer The top left buffer
- * @param topRightBuffer The top right buffer
- * @param bottomLeftBuffer The bottom left buffer
- * @param bottomRightBuffer The bottom right buffer
- */
-void init(uint8_t *rawBuffer, uint8_t *topLeftBuffer, uint8_t *topRightBuffer,
-          uint8_t *bottomLeftBuffer, uint8_t *bottomRightBuffer);
-/**
- * @brief Sets the offset for the corner viewers
- *
- * @param offset The offset in pixels
- */
-void setROIOffset(uint8_t offset);
-/**
- * @brief Sets the scale of the corner viewers
- *
- * @param scale The scale
- */
-void setROIScale(uint8_t scale);
-/**
- * @brief Sets the size of the corner viewers
- *
- * @param size The size in pixels
- */
-void setROISize(uint8_t size);
-/**
- * @brief Starts the streaming
- */
-void start();
-/**
- * @brief Stops the streaming and cleans up
- */
-void stop();
 }
--- src/streamer.cpp
+++ src/streamer.cpp
@@ -1,13 +1,14 @@
 #include "streamer.h"
+#include "util.h"
 #include <metavision/sdk/base/events/event_cd.h>
 #include <metavision/sdk/stream/camera.h>
-#include <omp-tools.h>
-#include <omp.h>
+#include <opencv2/core.hpp>
 #include <spdlog/spdlog.h>
+#include <thread>
+#include <opencv2/highgui.hpp>
 
 // Declare the buffers
-static cv::Mat rawBuffer, topLeftBuffer, topRightBuffer, bottomLeftBuffer,
-    bottomRightBuffer;
+static uint8_t *rawBuffer, *topLeftBuffer, *topRightBuffer, *bottomLeftBuffer, *bottomRightBuffer, *centerBuffer;
 
 // Declare the runtime variables
 static uint8_t ROIOffset = 0;
@@ -17,8 +18,30 @@ static uint8_t ROISize = 20;
 // The camera
 static Metavision::Camera *camera;
 
-void init(uint8_t *raw, uint8_t *topLeft, uint8_t *topRight,
-          uint8_t *bottomLeft, uint8_t *bottomRight) {}
+// Program status
+static bool running = false;
+
+void init(uint8_t *raw, uint8_t *topLeft, uint8_t *topRight, uint8_t *bottomLeft, uint8_t *bottomRight, uint8_t *center) {
+  rawBuffer = raw;
+  topLeftBuffer = topLeft;
+  topRightBuffer = topRight;
+  bottomLeftBuffer = bottomLeft;
+  bottomRightBuffer = bottomRight;
+  centerBuffer = center;
+  running = true;
+}
+
+uint8_t getROIOffset() {
+  return ROIOffset;
+}
+
+uint8_t getROIScale() {
+  return ROIScale;
+}
+
+uint8_t getROISize() {
+  return ROISize;
+}
 
 void setROIOffset(uint8_t offset) {
   ROIOffset = offset;
@@ -36,29 +59,79 @@ void setROISize(uint8_t size) {
 }
 
 void start() {
-  // Create the camera
-  spdlog::info("Starting the camera");
-  *camera = Metavision::Camera::from_first_available();
-  spdlog::info("Successfully started the camera");
+  // Start the camera
+  spdlog::info("Starting camera");
+  Metavision::Camera camera = Metavision::Camera::from_first_available();
+  spdlog::info("Successfully started camera");
 
-  // Callback to be called with events
-  auto callback = [&](const Metavision::EventCD *begin,
-                      const Metavision::EventCD *end) {
-// Loop through all the events
-#pragma omp parallel for
+  // Get the dimensions of the camera
+  int width = camera.geometry().get_width();
+  int height = camera.geometry().get_height();
+  spdlog::info("Width {}, Height {}", width, height);
+
+  // Create buffers for the image
+  cv::Mat *raw = new cv::Mat(height, width, CV_8UC3, rawBuffer);
+  
+  // Counter to keep track of the event rate
+  std::chrono::time_point start = std::chrono::high_resolution_clock::now();
+
+  // Worker to write to the buffer
+  auto worker = [&](const Metavision::EventCD *begin, const Metavision::EventCD *end) {
+    // Loop through all the events
     for (auto it = begin; it != end; ++it) {
       // Write into raw
-      cv::Vec3b *ptr = rawBuffer.ptr<cv::Vec3b>(it->y, it->x);
+      cv::Vec3b *ptr = raw->ptr<cv::Vec3b>(it->y, it->x);
       (*ptr)[0] = it->p * 255;
       (*ptr)[1] = (1 - it->p) * 255;
     }
   };
 
+  // Callback to be called with events
+  auto callback = [&](const Metavision::EventCD *begin, const Metavision::EventCD *end) {
+    long dt = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - start).count();
+
+    if ((dt - end->t) < 1e5) {
+      std::thread thread(worker, begin, end);
+      thread.detach();
+    }
+  };
+
   // Register the callback
   spdlog::info("Starting the camera stream");
-  camera->cd().add_callback(callback);
-  camera->start();
+  camera.cd().add_callback(callback);
+  camera.start();
   spdlog::info("Successfully started camera stream");
+
+  auto mainTask = [&]() {
+    while (running) {
+      uint8_t windowSize = ROISize * ROIScale;
+      cutRegion(*raw, {0, 0, ROISize, ROISize}, ROIScale).copyTo(*(new cv::Mat(windowSize, windowSize, CV_8UC1, topLeftBuffer)));
+      cutRegion(*raw, {width - ROISize, 0, ROISize, ROISize}, ROIScale).copyTo(*(new cv::Mat(windowSize, windowSize, CV_8UC1, topRightBuffer)));
+      cutRegion(*raw, {0, height - ROISize, ROISize, ROISize}, ROIScale).copyTo(*(new cv::Mat(windowSize, windowSize, CV_8UC1, bottomLeftBuffer)));
+      cutRegion(*raw, {width - ROISize, height - ROISize, ROISize, ROISize}, ROIScale).copyTo(*(new cv::Mat(windowSize, windowSize, CV_8UC1, bottomRightBuffer)));
+      cutRegion(*raw, {(width - ROISize) / 2, (height - ROISize) / 2, ROISize, ROISize}, ROIScale).copyTo(*(new cv::Mat(windowSize, windowSize, CV_8UC1, centerBuffer)));
+
+      // Fade
+      *raw /= 2;
+    }
+    spdlog::info("Main loop shut down");
+  };
+
+  // std::thread mainTaskThread(mainTask);
+  // mainTaskThread.detach();
+
+  // Loop while running
+  spdlog::info("Started main loop");
 }
 
-void stop() {}
+void stop() {
+  spdlog::info("Exiting");
+
+  // Shut down the camera
+  spdlog::info("Shutting down the camera");
+  camera->stop();
+  spdlog::info("Successfully shut down the camera");
+
+  spdlog::info("Shutting down main loop");
+  running = false;
+}
